9982abafda6eca251f1832984f44e8f928838338
Tom Shea <tom@shea.at>
2015-12-05 04:07:01 +0000

    Add codemirror

--- source/index.js
+++ source/index.js
@@ -1,7 +1,10 @@
+var CodeMirror = require('codemirror');
 function init() {
   console.log("hello world");
 }
 
 window.addEventListener("load", function() {
+  var textarea = document.getElementById("text");
+  var codeMirrorInstance = CodeMirror.fromTextArea(textarea);
   init();
 });
