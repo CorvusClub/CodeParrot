4f534d7f0bf20b1c64238929b678feed0e386bef
Tom Shea <tom@shea.at>
2015-12-05 23:39:53 +0000

    Revert to much simpler menu

--- source/menu.js
+++ source/menu.js
@@ -1,51 +1,10 @@
 class Menu {
   constructor(element) {
     this.element = element;
-    this.element.addEventListener("click", this.onClick.bind(this));
-    this.element.addEventListener("mousemove", (event) => {
-      let target = event.target;
-      if(target.nodeName === "LABEL") {
-        target = target.nextElementSibling;
-      }
-      if(target.nodeName === "UL" && this.open.indexOf(target) === -1) {
-        this.select(target);
-      }
+    this.themeSelector = this.element.querySelector(".theme");
+    this.themeSelector.addEventListener("change", (event) => {
+      document.body.className = this.themeSelector.value;
     });
-    document.body.addEventListener("click", (event) => {
-      if(!this.element.contains(event.target)) {
-        this.open.forEach(function(menu) { menu.classList.remove("open") });
-        this.open = [];
-      }
-    });
-    this.open = [];
-  }
-  select(target) {
-    if(this.open.length > 0) {
-      if(this.open.indexOf(target) != -1) {
-        target.classList.remove("open");
-        this.open.splice(this.open.indexOf(target));
-        return;
-      }
-      this.open.forEach((openMenu) => {
-        if(openMenu.parentElement.parentElement === target.parentElement.parentElement) {
-          openMenu.classList.remove("open");
-          this.open.splice(this.open.indexOf(openMenu));
-        }
-      });
-    }
-    target.classList.add("open");
-    this.open.push(target);
-  }
-  onClick(event) {
-    let target = event.target;
-    if(target.nodeName === "LABEL") {
-      target = target.nextElementSibling;
-    }
-    if(target.nodeName === "LI") {
-      // do menu item click stuff
-      return;
-    }
-    this.select(target);
   }
 }
 
