271c25d35f49904f5cc6c87bcc51e1be8dc91600
Tom Shea <tom@shea.at>
2015-12-05 06:01:11 +0000

    Use javacript mode by default

--- source/index.js
+++ source/index.js
@@ -1,10 +1,16 @@
 var CodeMirror = require('codemirror');
+require('codemirror/mode/javascript/javascript.js');
 function init() {
   console.log("hello world");
 }
 
 window.addEventListener("load", function() {
   var textarea = document.getElementById("text");
-  var codeMirrorInstance = CodeMirror.fromTextArea(textarea, {theme: "seti"});
+  var codeMirrorInstance = CodeMirror.fromTextArea(textarea, {
+    theme: "seti",
+    lineNumbers: true,
+    autofocus: true,
+    mode: "javascript"
+  });
   init();
 });
