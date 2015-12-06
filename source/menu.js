19c5cbefe6a8570612795faa4512a52641e7dd33
Tom Shea <tom@shea.at>
2015-12-06 01:03:29 +0000

    Don't try to theme the interface

--- source/menu.js
+++ source/menu.js
@@ -5,7 +5,6 @@ class Menu {
     this.themeSelector = this.element.querySelector(".theme");
     this.themeSelector.addEventListener("change", (event) => {
       let theme = this.themeSelector.value;
-      document.body.className = theme;
       this.codeMirror.setOption({theme});
     });
   }
